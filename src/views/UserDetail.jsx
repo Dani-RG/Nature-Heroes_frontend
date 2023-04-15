66f9c739cbc638425f21cf298673ec7c91db83fd
Daniel-RG <21danielrondong@gmail.com>
2023-04-15 11:25:06 +0200

    Adds level on userdetail view

--- src/views/UserDetail.jsx
+++ src/views/UserDetail.jsx
@@ -7,25 +7,11 @@ import CircularProgress from '../components/CircularProgress';
 export default function UserDetail() {
   const { isLoggedIn, user } = useContext(AuthContext);
   const CircleSize = 200;
-  //const [progress, setProgress] = useState(0);
-  //const [level, setLevel] = useState(1);
-  
-  // const handleProgressChange = () => {
-  //   setLevel(Math.floor(user.donated_total/100));
-  //   setProgress(user.donated_total%100);
-  // }
-
-  // useEffect(() => {
-  //   handleProgressChange();
-  //   // eslint-disable-next-line
-  // }, [])
-
-  const level = (Math.floor(user.donated_total/100));
 
   return (
     <div>
-      {!user && !level && <p>Loading...</p>}
-      {user && level && <div>
+      {!user && <p>Loading...</p>}
+      {user &&  <div>
         <div>
           <img src={user.image} alt={user.username}/>
           <h2>Username:</h2>
@@ -42,7 +28,7 @@ export default function UserDetail() {
           <h3>{user.donated_total}</h3>
           <CircularProgress progress={user.donated_total%100} size={CircleSize} />
           <h3>Hero Level:</h3>
-          <h3>{level}</h3>
+          <h3>{Math.floor(user.donated_total/100)}</h3>
           {/* <h3>{Math.floor(user.donated_total/100)}</h3> */}
         </div>
       </div>}
